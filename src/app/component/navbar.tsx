245901829b719fc986ab2783f792908e4fc4c580
Vaskar-Bhattacharjee <vaskarbhattacharjee03@gmail.com>
2025-11-17 11:17:06 +0600

    Add Navbar component and implement theme switching functionality
    - Introduced Navbar component for site navigation.
    - Added 'next-theme' and 'next-themes' dependencies for theme management.
    - Enhanced Home page with icons and theme toggle feature.
    - Updated global styles for dark mode support.

---
+++ src/app/component/navbar.tsx
@@ -0,0 +1,17 @@
+
+function Navbar() {
+    const navlinks = [
+        { name: 'Home', href: '/' },
+        { name: 'About', href: '/about' },
+        { name: 'Contact', href: '/contact' },
+    ]
+  return (
+    <div>
+        <nav className="bg-">
+
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
